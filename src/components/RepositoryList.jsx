b0f284bc63691fba80ec429f0a0afc50bc674b13
hernany <hernanytec777@gmail.com>
2021-12-15 20:08:51 -0300

    add component repositorylist

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar repositórios</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
